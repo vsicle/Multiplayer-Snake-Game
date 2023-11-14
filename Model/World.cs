45ed9b48879cac5f0a31994cc65269ee9cdca934
Vasko <vvassilev515@gmail.com>
2023-11-14 15:14:05 -0700

    started world class

--- Model/World.cs
+++ Model/World.cs
@@ -1,10 +1,32 @@
 ﻿namespace Model
+    
 {
     /// <summary>
     /// This class contains all the objects the Server sends.
     /// </summary>
     public class World
     {
+        private Dictionary<int, Snake> snakes;
+        private Dictionary<int, Powerup> powerups;
+        private Dictionary<int, Wall> walls;
 
+        /// <summary>
+        /// create a blank world
+        /// </summary>
+        public World()
+        {
+            snakes = new Dictionary<int, Snake>();
+            powerups = new Dictionary<int, Powerup>();
+            walls = new Dictionary<int, Wall>();
+        }
+
+        /// <summary>
+        /// update the world, given a JSON string from the server
+        /// </summary>
+        /// <param name="JsonString"></param>
+        public void UpdateWorld(string JsonString)
+        {
+
+        }
     }
 }
