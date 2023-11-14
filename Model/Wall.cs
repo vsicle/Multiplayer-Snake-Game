e7d81c216fe6ce4b0c50cab32200017e14ff83db
Vasko <vvassilev515@gmail.com>
2023-11-14 14:21:07 -0700

    created wall class

---
+++ Model/Wall.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Wall
+    {
+
+        public int WallId {get;set;}
+        public Vector2D P1 {get;set;}
+        public Vector2D P2 {get;set;}
+
+        public Wall(int _wallId, Vector2D _p1, Vector2D _p2)
+        {
+            WallId = _wallId;
+            P1 = _p1;
+            P2 = _p2;
+        }
+    }
+
+    
+}
