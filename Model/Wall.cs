560979a07384291cc215dfcb958fac68dfc6a33e
Vasko <vvassilev515@gmail.com>
2023-11-14 14:22:42 -0700

    fixed wall class

--- Model/Wall.cs
+++ Model/Wall.cs
@@ -9,15 +9,15 @@ namespace Model
     public class Wall
     {
 
-        public int WallId {get;set;}
-        public Vector2D P1 {get;set;}
-        public Vector2D P2 {get;set;}
+        public int wall {get;set;}
+        public Vector2D p1 { get;set;}
+        public Vector2D p2 {get;set;}
 
         public Wall(int _wallId, Vector2D _p1, Vector2D _p2)
         {
-            WallId = _wallId;
-            P1 = _p1;
-            P2 = _p2;
+            wall = _wallId;
+            p1 = _p1;
+            p2 = _p2;
         }
     }
 
