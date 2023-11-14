43cfd859af9b1c28a2c3082554a24367870c8f5a
Vasko <vvassilev515@gmail.com>
2023-11-14 16:52:30 -0700

    issue with JSON, needs fixing

--- Model/World.cs
+++ Model/World.cs
@@ -1,4 +1,8 @@
-﻿namespace Model
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace Model
     
 {
     /// <summary>
@@ -9,15 +13,16 @@
         private Dictionary<int, Snake> snakes;
         private Dictionary<int, Powerup> powerups;
         private Dictionary<int, Wall> walls;
-
+        private int size;
         /// <summary>
         /// create a blank world
         /// </summary>
-        public World()
+        public World(int worldSize)
         {
             snakes = new Dictionary<int, Snake>();
             powerups = new Dictionary<int, Powerup>();
             walls = new Dictionary<int, Wall>();
+            size = worldSize;
         }
 
         /// <summary>
@@ -26,6 +31,50 @@
         /// <param name="JsonString"></param>
         public void UpdateWorld(string JsonString)
         {
+
+            // figure out what object is being created or updated
+            // likely using the first word in the JSON
+            JsonDocument JDoc = JsonDocument.Parse(JsonString);
+
+            //TODO: fix JSON issue
+
+            var JsonName = JDoc.RootElement.EnumerateObject();
+            string objectKind = JsonName.ElementAt(0).Name;
+
+            // TA said to use rootelement.TryGetValue
+
+
+            switch(objectKind)
+            {
+                case "wall":
+                    Wall? tempWall = JsonSerializer.Deserialize<Wall>(objectKind);
+                    if(tempWall != null)
+                    {
+                        walls.Add(tempWall.wall, tempWall);
+                    }
+                    break;
+                case "snake":
+                    Snake? tempSnake = JsonSerializer.Deserialize<Snake>(objectKind);
+                    if (tempSnake != null)
+                    {
+                        snakes.Add(tempSnake.snake, tempSnake);
+                    }
+                    break;
+                case "power":
+                    Powerup? tempPower = JsonSerializer.Deserialize<Powerup>(objectKind);
+                    if (tempPower != null)
+                    {
+                        powerups.Add(tempPower.power, tempPower);
+                    }
+                    break;
+                default:
+                    Debug.WriteLine("Failed to match/find type of JSON object");
+                    break;
+            }
+            
+
+        
+
 
         }
     }
